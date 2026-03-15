08333010b2a62a02b757eaf7ab0cbea90e97ea9d
ryanw84 <ryanweavers@gmail.com>
2026-03-15 21:50:44 +0000

    feat(views): setting up first view

---
+++ MvcMoviePractice/Views/HelloWorld/Index.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MvcMoviePractice.Controllers.Views.HelloWorld
+{
+    public class Index : PageModel
+    {
+        private readonly ILogger<Index> _logger;
+
+        public Index(ILogger<Index> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet() { }
+    }
+}
