8a34c1726e7a5880b1d705b0f6713cda59f638fb
ryanw84 <ryanweavers@gmail.com>
2026-03-25 21:27:03 +0000

    Quick push

--- MvcMoviePractice/Controllers/MoviesController.cs
+++ MvcMoviePractice/Controllers/MoviesController.cs
@@ -101,6 +101,7 @@ namespace MvcMoviePractice.Controllers
                         return NotFound();
                     }
                     else //
+                    //
                     {
                         throw;
                     }
