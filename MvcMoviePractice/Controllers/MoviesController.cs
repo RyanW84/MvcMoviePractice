fc4438eab839f315fec61cf03bc421407e4b6b31
ryanw84 <ryanweavers@gmail.com>
2026-03-28 21:30:11 +0000

    feat(genre): Genre filtering and searching

--- MvcMoviePractice/Controllers/MoviesController.cs
+++ MvcMoviePractice/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MvcMoviePractice.Data;
 using MvcMoviePractice.Models;
@@ -10,13 +11,15 @@ namespace MvcMoviePractice.Controllers
         private readonly MvcMoviePracticeContext _context = context;
 
         // GET: Movies
-        public async Task<IActionResult> Index(string searchString)
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
             if (_context.Movie == null)
             {
-                return Problem("Entity Set 'MvcMovieContext.Movie' is null");
+                return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
             }
 
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie orderby m.Genre select m.Genre;
             var movies = from m in _context.Movie select m;
 
             if (!string.IsNullOrEmpty(searchString))
@@ -24,7 +27,18 @@ namespace MvcMoviePractice.Controllers
                 movies = movies.Where(s => s.Title!.ToUpper().Contains(searchString.ToUpper()));
             }
 
-            return View(await movies.ToListAsync());
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            var movieGenreVM = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync(),
+            };
+
+            return View(movieGenreVM);
         }
 
         // POST: Movies
